09c46a74c7a4981678abe7437a4b11328d514c16
Manish Bansal <qualityking@gmail.com>
2015-10-23 15:42:11 +0530

    Update Assertion.java

--- Assertion.java
+++ Assertion.java
@@ -38,30 +38,41 @@ public class Assertion {
 		return true;
 	}
 
-	static void printDetails(boolean expected, Object val1, Object val2) {
+	static void printDetails(boolean expected, Object expectedVal, Object actualVal) {
 		if (expected) {
-			System.err.println("Expected Value : " + val1);
+			System.err.println("Expected Value : " + expectedVal);
 		} else {
-			System.err.println("Expected Value Should Not : " + val1);
+			System.err.println("Expected Value Should Not : " + expectedVal);
 		}
-		System.err.println("Actual Value   : " + val2);
+		System.err.println("Actual Value   : " + actualVal);
 		sleep(100);
 		throw new RuntimeException("Assertion Failed");
 	}
 
 	public static boolean test(boolean expected, boolean condition, String customMessage) {
 		if (condition == expected) {
+			Log.info("Assertion Passed > " + customMessage);
 			return true;
 		} else {
 			if (customMessage == "") {
-				System.err.print("Assertion Failed ");
+				if(expected){
+					System.err.print("Assertion Failed > Should But it is not > ");
+				}else {
+					System.err.print("Assertion Failed > ShouldNot But it is > ");	
+				}
+				
+				
 			} else {
-				System.err.println("Assertion Failed : " + customMessage);
+				if(expected){
+					System.err.println("Assertion Failed > Should > " + customMessage + " > But it is not.");
+				}else {
+					System.err.println("Assertion Failed > ShouldNot > " + customMessage + " > But it is.");	
+				}
+				
 			}
 			sleep(100);
 			return false;
 		}
-
 	}
 
 	private static void sleep(long milliseconds) {
